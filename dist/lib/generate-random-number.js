67d9b1f35fb13433a150eb5f240f41323ebcf4cf
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-12-04 17:56:20 +0430

    refactoring js-generate-password to type save

--- dist/lib/generate-random-number.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateRandomNumbers = void 0;
-const generateRandomNumbers = (upperLimit) => {
-    return Math.floor(Math.random() * upperLimit);
-};
-exports.generateRandomNumbers = generateRandomNumbers;
-// module.exports = generateRandomNumbers;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { generatePswd } from "./lib/password-generator";
-import { checkError } from "./lib/check-options-error";
-import { processOption } from "./lib/process-options";
-import { GenerateOptions } from "./lib/types";
+import { generatePswd } from './lib/password-generator'
+import { checkError } from './lib/check-options-error'
+import { processOption } from './lib/process-options'
+import { GenerateOptions } from './types'
 
 function GeneratePassword(options?: GenerateOptions) {
-  let pswd;
+  let pswd
   if (options) {
-    options = processOption(options);
+    options = processOption(options)
 
-    checkError(options);
+    checkError(options)
 
-    pswd = generatePswd(options);
-    return pswd;
+    pswd = generatePswd(options)
+    return pswd
   } else {
     let opt = {
       length: 10,
@@ -19,21 +19,20 @@ function GeneratePassword(options?: GenerateOptions) {
       uppercase: true,
       numbers: true,
       symbols: false,
-      exclude: "",
+      exclude: '',
       minLengthLowercase: 1,
       minLengthUppercase: 1,
       minLengthNumbers: 1,
       minLengthSymbols: 0,
-    };
-    options = processOption(opt);
+    }
+    options = processOption(opt)
 
-    checkError(options);
+    checkError(options)
 
-    pswd = generatePswd(options);
-    return pswd;
+    pswd = generatePswd(options)
+    return pswd
   }
 }
 
-// console.log(GeneratePassword({ minLengthLowercase: 4 }));
-export { GeneratePassword };
-export default { GeneratePassword };
+// Exporting GeneratePassword as the default export and a named export
+export { GeneratePassword, type GenerateOptions }
